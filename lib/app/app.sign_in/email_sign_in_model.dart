06bfea47a187ae1698ab2b8ebc59ce01ea64db9b
Mertcan Toptaş <mtoptas@invio.com>
2021-01-02 19:18:32 +0300

    refactoring with blog in email page

--- lib/app/app.sign_in/email_sign_in_model.dart
+++ lib/app/app.sign_in/email_sign_in_model.dart
@@ -1,6 +1,8 @@
+import 'package:time_tracker/app/app.sign_in/validators.dart';
+
 enum EmailSignInFormType { signIn, register }
 
-class EmailSignInModel {
+class EmailSignInModel with EmailAndPasswordValidators {
   EmailSignInModel(
       {this.email = '',
       this.password = '',
@@ -12,6 +14,34 @@ class EmailSignInModel {
   final EmailSignInFormType formType;
   final bool isLoading;
   final bool submitted;
+
+  String get primaryButtonText {
+    return formType == EmailSignInFormType.signIn
+        ? 'Sign in'
+        : 'Create an account';
+  }
+
+  String get secondaryButtonText {
+    return formType == EmailSignInFormType.signIn
+        ? 'Need an account? Register'
+        : 'Have an account? Sign in';
+  }
+
+  bool get canSubmit {
+    return emailValidator.isValid(email) &&
+        passwordValidator.isValid(password) &&
+        !isLoading;
+  }
+
+  String get passwordErrorText {
+    bool showErrorText = submitted && !passwordValidator.isValid(password);
+    return showErrorText ? invalidEmailErrorText : null;
+  }
+
+  String get emailErrorText {
+    bool showErrorText = submitted && !emailValidator.isValid(email);
+    return showErrorText ? invalidEmailErrorText : null;
+  }
 
   EmailSignInModel copyWith(
       {String email,
